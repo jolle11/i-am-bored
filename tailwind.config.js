9494cf7c972672b63d207372fbcc30aaa1ed4efb
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2022-10-11 17:54:09 +0200

    Added custom blues

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 module.exports = {
-  content: ['index.html','./src/**/*.{js,jsx,ts,tsx,vue,html}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: ["index.html", "./src/**/*.{js,jsx,ts,tsx,vue,html}"],
+	theme: {
+		extend: {
+			colors: {
+				matchBlue: {
+					700: "#6E85FD",
+					800: "#5A74FD",
+					900: "#4764fd",
+					1000: "#3454FD",
+					1100: "#2043FD",
+				},
+			},
+		},
+	},
+	plugins: [],
+};
