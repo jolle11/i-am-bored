4646fed2aedfd10193a4e9f66b386aed269c4570
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2022-10-11 17:53:19 +0200

    Added font

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePluginFonts } from "vite-plugin-fonts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePluginFonts({
+      google: {
+        families: [
+          {
+            name: "Silkscreen",
+            styles: "wght@400;700",
+            defer: true,
+            // font-family: 'Silkscreen', cursive;
+          },
+        ],
+      },
+    }),
+  ],
 });
