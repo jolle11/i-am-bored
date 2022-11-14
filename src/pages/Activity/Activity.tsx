8b08df16def23128722afc80a34dbd37777268c0
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2022-11-14 19:23:53 +0100

    Deleted unnecessary things

--- src/pages/Activity/Activity.tsx
+++ src/pages/Activity/Activity.tsx
@@ -1,29 +1,13 @@
-import { useState } from "react";
-import { Dispatch, SetStateAction } from "react";
-import { useLocation } from "react-router-dom";
-
 import { WantAnother, ActivityResult, Footer } from "../../components";
 
-interface IProps {
-  isLoading: boolean;
-  setIsLoading?: Dispatch<SetStateAction<boolean>>;
-}
-
 const Activity = () => {
-  const location = useLocation();
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-
-  return (
-    <div className="flex flex-col items-center justify-center">
-      <WantAnother toggleLoader={setIsLoading} />
-      <ActivityResult
-        data={location.state}
-        loader={isLoading}
-        toggleLoader={setIsLoading}
-      />
-      <Footer />
-    </div>
-  );
+	return (
+		<div className="flex flex-col items-center justify-center">
+			<WantAnother />
+			<ActivityResult />
+			<Footer />
+		</div>
+	);
 };
 
 export default Activity;
