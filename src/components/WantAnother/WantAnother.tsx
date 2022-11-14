581f1382612161f03e60a8c42e50cb0c948761df
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2022-11-14 19:22:18 +0100

    Used RTK Query

--- src/components/WantAnother/WantAnother.tsx
+++ src/components/WantAnother/WantAnother.tsx
@@ -1,35 +1,47 @@
 import { useNavigate } from "react-router-dom";
+import { useAppDispatch, useAppSelector } from "../../app/hooks";
+import { reset } from "../../features/activities/activitySlice";
 
-interface Props {
-  toggleLoader?: any;
-}
+import { useFetchActivitiesQuery } from "../../features/activities/activityApiSlice";
 
-const WantAnother = ({ toggleLoader }: Props) => {
-  const navigate = useNavigate();
+const WantAnother = () => {
+	const activity = useAppSelector((state) => state.activity);
+	const dispatch = useAppDispatch();
 
-  const handleClickBack = () => {
-    navigate("/");
-  };
-  const handleClickRefresh = () => {
-    toggleLoader(true);
-  };
+	const navigate = useNavigate();
 
-  return (
-    <div className="flex items-center justify-center">
-      <button
-        onClick={handleClickBack}
-        className="m-2 rounded-full bg-matchBlue-900 p-3 text-white shadow-xl transition-all hover:bg-matchBlue-1100"
-      >
-        Go Back
-      </button>
-      <button
-        onClick={handleClickRefresh}
-        className="margin-1 rounded-full bg-matchBlue-900 p-3 text-white shadow-xl transition-all hover:bg-matchBlue-1100"
-      >
-        I want something else
-      </button>
-    </div>
-  );
+	const handleClickBack = () => {
+		dispatch(reset());
+		navigate("/");
+	};
+
+	const { refetch } = useFetchActivitiesQuery({
+		participants: activity.participants!,
+		type: activity.type,
+		price: activity.price!,
+		accessibility: activity.accessibility!,
+	});
+
+	function handleRefetchOne() {
+		refetch();
+	}
+
+	return (
+		<div className="flex items-center justify-center">
+			<button
+				onClick={handleClickBack}
+				className="m-2 rounded-full bg-matchBlue-900 p-3 text-white shadow-xl transition-all hover:bg-matchBlue-1100"
+			>
+				Go Back
+			</button>
+			<button
+				onClick={handleRefetchOne}
+				className="margin-1 rounded-full bg-matchBlue-900 p-3 text-white shadow-xl transition-all hover:bg-matchBlue-1100"
+			>
+				I want something else
+			</button>
+		</div>
+	);
 };
 
 export default WantAnother;
